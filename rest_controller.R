f5a2bfc8b6a22b4837bf03e5611e35fb7dc9630b
Nick Solomon <nicksolomon10@gmail.com>
2019-06-16 20:01:47 -0400

    make names a bit less weird

--- rest_controller.R
+++ rest_controller.R
@@ -3,5 +3,6 @@ source("runtime_functions.R")
 # Set an endpoint to return a pet name
 #* @get /names
 get_names <- function(){
-  generate_many_names(20, model, character_lookup, max_length)
+  generate_many_names(20, model, character_lookup, max_length, temperature = .8)
 }
+
